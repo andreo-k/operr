20442ba3497fb655973268ebb238f967e3d0311d
andrey-k <444221@gmail.com>
2018-11-09 09:15:18 -0800

    redirect home route to todo-item

--- src/main/webapp/app/home/home.route.ts
+++ src/main/webapp/app/home/home.route.ts
@@ -4,9 +4,11 @@ import { HomeComponent } from './';
 
 export const HOME_ROUTE: Route = {
     path: '',
-    component: HomeComponent,
-    data: {
-        authorities: [],
-        pageTitle: 'Welcome, Java Hipster!'
-    }
+    redirectTo: 'todo-item',
+    pathMatch: 'full'
+    // component: HomeComponent,
+    // data: {
+    //     authorities: [],
+    //     pageTitle: 'Welcome, Java Hipster!'
+    // }
 };
